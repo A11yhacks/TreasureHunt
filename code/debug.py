7a39fa63dc5beef59de73f6155f204baffc62023
bmustill-rose <ben.mustill-rose@bbc.co.uk>
2016-11-02 16:22:34 +0000

    "Propper" indentation

--- code/debug.py
+++ code/debug.py
@@ -17,38 +17,38 @@ iBeaconMajor="1000" #The major ID of your beacons (has to be a string otherwise 
 #Each beacon broadcasts several pieces of information which we're interested in. This method helps separate them out
 #Todo: better exception handling
 def parsePacket(packet):
- outputToReturn={} #A dictionary that will (hopefully) contain the output that we want to return
- parsedPacket=packet.split(",") #Create a list that contains the individual parts of the packet using a "," as the delimiter
- try: #Put this bit in an exception in case parsedPacket isn't an iBeacon (the below is very iBeacon specific code)
-  outputToReturn["BDAddr"]=parsedPacket[0] #The Bluetooth address of the iBeacon
-  outputToReturn["UUID"]=parsedPacket[1] #The UUID of the iBeacon
-  outputToReturn["major"]=parsedPacket[2] #The major ID of the iBeacon
-  outputToReturn["minor"]=parsedPacket[3] #The minor ID of the iBeacon
-  #We receive 2 more pieces of data but it is not obvious what they are. Testing suggests that they are not TX / RSSI for example - we'll just ignore them for now
- except:
-  raise
- else:
-  return outputToReturn
+    outputToReturn={} #A dictionary that will (hopefully) contain the output that we want to return
+    parsedPacket=packet.split(",") #Create a list that contains the individual parts of the packet using a "," as the delimiter
+    try: #Put this bit in an exception in case parsedPacket isn't an iBeacon (the below is very iBeacon specific code)
+        outputToReturn["BDAddr"]=parsedPacket[0] #The Bluetooth address of the iBeacon
+        outputToReturn["UUID"]=parsedPacket[1] #The UUID of the iBeacon
+        outputToReturn["major"]=parsedPacket[2] #The major ID of the iBeacon
+        outputToReturn["minor"]=parsedPacket[3] #The minor ID of the iBeacon
+        #We receive 2 more pieces of data but it is not obvious what they are. Testing suggests that they are not TX / RSSI for example - we'll just ignore them for now
+    except:
+        raise
+    else:
+        return outputToReturn
 
 #Try to create a socket
 try:
- sock = bluez.hci_open_dev(deviceID)
- print("Socket created successfully!")
+    sock = bluez.hci_open_dev(deviceID)
+    print("Socket created successfully!")
 except:
- print("error accessing bluetooth device! Have you set deviceID correctly? Can not continu.")
- sys.exit(1)
+    print("error accessing bluetooth device! Have you set deviceID correctly? Can not continu.")
+    sys.exit(1)
 
 #Set everything up in preperation for performing the scan
 blescan.hci_le_set_scan_parameters(sock)
 blescan.hci_enable_le_scan(sock)
 
 while True:
- results=blescan.parse_events(sock, 10) #socket, maxNumberOfResults (default is 100)
- for packet in results:
-  parsed=parsePacket(packet)
-  try:
-   if parsed["UUID"] == iBeaconUUID and parsed["major"] == iBeaconMajor and parsed["minor"] != lastEncounterediBeacon: #If we've encountered an Estimote iBeacon that has our major Id but is not the last one we delt with
-    print("Found major %s minor %s" % (parsed["major"], parsed["minor"]))
-    lastEncounterediBeacon=parsed["minor"]
-  except:
-   pass
+    results=blescan.parse_events(sock, 10) #socket, maxNumberOfResults (default is 100)
+    for packet in results:
+        parsed=parsePacket(packet)
+        try:
+            if parsed["UUID"] == iBeaconUUID and parsed["major"] == iBeaconMajor and parsed["minor"] != lastEncounterediBeacon: #If we've encountered an Estimote iBeacon that has our major Id but is not the last one we delt with
+                print("Found major %s minor %s" % (parsed["major"], parsed["minor"]))
+                lastEncounterediBeacon=parsed["minor"]
+        except:
+            pass
